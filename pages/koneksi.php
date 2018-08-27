c0db95fe11dd52b09aeeead651c1e83e6a635306
barikly <barikly@hitachi-ebworx.com>
2018-08-27 15:20:00 +0700

    update koneksi.php

--- pages/koneksi.php
+++ pages/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$user="root";
-$pass="";
-$host="localhost";
-$db="rjm";
+$user="bf046e5b8b3638";
+$pass="e4aff0f1";
+$host="us-cdbr-iron-east-01.cleardb.net";
+$db="heroku_38e9fe5c86d6f7b";
 
 $config=mysql_connect($host,$user,$pass);
 $connect=mysql_select_db($db,$config) or die(mysql_error());
